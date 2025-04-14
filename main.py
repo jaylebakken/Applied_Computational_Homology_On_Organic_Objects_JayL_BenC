6df6bbc2674f398038a6d71261adfe3948a06c0a
jaylebakken <123206292+jaylebakken@users.noreply.github.com>
2025-04-14 10:11:35 -0500

    publish

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import math
+
 from PIL import Image
 import numpy as np
+import gudhi.cover_complex as gud
+import os
 
-
-img = Image.open("Dataset/POLLEN73S/acrocomia_aculeta/Figura1.TIF")
-img = img.convert('L')
-imat = np.asarray(img)
-imat = np.where(imat<120, imat,255)
+for i in range(150):
+    img = Image.open("Dataset/POLLEN73S/acrocomia_aculeta/Figura1.TIF")
+    img = img.convert('L')
+    imat = np.asarray(img)
+    imat = np.where(imat<(50+i), imat,255)
+    img = Image.fromarray(imat,mode="L")
+    #img.save("sampleImages/sampleimg_{}.png".format(int(i)))
 print(imat)
-img = Image.fromarray(imat,mode="L")
-img.save("ooohahhh.png")
+
+#mp = gud.MapperComplex()
+#mp.fit(X=imat)
+
