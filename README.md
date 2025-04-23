6d987bfb6f8d83b9358fa7d8d27568342934f2e2
jaylebakken <123206292+jaylebakken@users.noreply.github.com>
2025-04-22 21:08:02 -0500

    Commented and cleaned up a lil

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Applied_Computational_Homology_On_Organic_Objects_JayL_BenC
  ACHOOO!
 DATASET FROM : POLLEN73S
+Ben Croutteau + Jay Lebakken, Lori Ziegelmeier
 
-
-
+# Current model outline
+-Converts image to black and white
+-Extracts any points above one standard deviation from the mean contrast, turns into point cloud
+-Computes Rips complex persistence for all images
+ 
--- main.py
+++ main.py
@@ -6,21 +6,30 @@ import gudhi
 from gudhi.sklearn.rips_persistence import RipsPersistence as rp
 
 def getCloud(image_ndArray):
+    ##parameters, 2d Nd Array representing an image
+    ##returns an array [x,y] representing a point cloud
     points = np.argwhere(image_ndArray<255)
     x = points[:, 1]
     y = points[:, 0]
     return [x,y]
     
 def filter(path):
+    #input: path of image
+    #returns a filtered version of image
     img = Image.open(path)
+    #Black and white conversion
     img = img.convert('L')
     imat = np.asarray(img)
-    imat = np.where(imat<(np.mean(imat)-np.std(imat)), imat,255)
+    #filter image to points outside of standard deviation of image contrast
+    imat = np.where(imat<(np.mean(imat)-np.std(imat)), imat,255) 
+    #Optional image saving:
     #img = Image.fromarray(imat,mode="L")
     #img.save("sampleImages/sampleimg_{}.png".format(int(i)))
     return imat
 
 def showPoints(point_Cloud):
+    #Parameter: [x,y] ndarray representing point cloud
+    #Plots point cloud
     x = point_Cloud[0]
     y = point_Cloud[1]
     plt.figure(figsize=(6, 6))
@@ -31,16 +40,19 @@ def showPoints(point_Cloud):
     plt.xlabel("x")
     plt.ylabel("y")
     plt.show()
-def computePD(filtered_image):
-    rips = rp([0,1,2], input_type='point cloud', num_collapses='auto', homology_coeff_field=11, n_jobs=None)
+
+def computePD(filtered_image, dimensions=[0,1,2], input_type_='point cloud', homo_field=11):
+    rips = rp(homology_dimensions=dimensions, input_type= input_type_, num_collapses='auto', homology_coeff_field=homo_field, n_jobs=None)
     diag = rips.transform([filtered_image])
     return diag
 
 def showPD(persistance_matrix,species):
+    ##Param: persistance_matrix as 2d ndarray, species as string
+    ##Plots persistance diagram
     ##unwrap PD
     PD = persistance_matrix[0]
     ##
-    colors = ['BLUE','RED','GREEN']
+    colors = ['BLUE','RED','GREEN'] 
     for i in range(0,3):    
         xx = PD[i][:,0]
         yy = PD[i][:,1]    
@@ -51,8 +63,10 @@ def showPD(persistance_matrix,species):
     plt.legend()
     plt.show()
 
+    ##TODO: complete bottleneck distance method
+
 def main():
-    img = filter("Dataset/POLLEN73S/acrocomia_aculeta/Figura2.TIF")
+    img = filter("Dataset/POLLEN73S/tapirira_guianensis/tapirira_guianensis_2.jpg")
     points = getCloud(img)
     showPoints(points)
     diag = computePD(img)
