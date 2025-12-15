fafd0b967cedf8d8090bef82aa790c3915a062a1
jaylebakken <123206292+jaylebakken@users.noreply.github.com>
2025-12-15 15:53:25 -0600

    Added functionality with making, with CALC FFD, Contrast filtering and point cloud creation

--- Mask.py
+++ Mask.py
@@ -6,20 +6,56 @@ import numpy as np
 import cv2
 
 
-def showPollen():
+def getCloud(image_ndArray):
+    ##parameters, 2d Nd Array representing an image
+    #epsilon, filtration value for the point cloud, default=100
+    ##returns an array [x,y] representing a point cloud
+    points = np.argwhere(image_ndArray > (np.mean(image_ndArray)+0.85*np.std(image_ndArray)))
+    x = points[:, 1]
+    y = points[:, 0]
+    return [x, y]
+
+def showPoints(point_Cloud):
+    # Parameter: [x,y] ndarray representing point cloud
+    # Plots point cloud
+    x = point_Cloud[0]
+    y = point_Cloud[1]
+    plt.figure(figsize=(6, 6))
+    plt.scatter(x, y, s=1, color='black')
+    plt.gca().invert_yaxis()
+    plt.axis('equal')
+    plt.title(" Edge Detection Pixels Point Cloud")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
+
+def showPollen(crop=20):
     path = "Dataset/"
     dataset = os.listdir(path)
+    #parse data
     for folder in dataset:
         currentfolder = os.listdir(path+folder + "/")
         for species in currentfolder:
             sample = os.listdir(path + folder + "/" + species + "/")
             img = cv2.imread(path + folder + "/" + species + "/"+sample[0])
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+            #show unfiltered image
             cv2.imshow("img", img)
+            #boost contrast with clahe
+            img = clahe.apply(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
             cv2.waitKey(0)
-            mask = fractional_edge_dft(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 0.75)
+            # show filtered image
+            mask = fractional_edge_dft(img, 1)
             cv2.imshow("edge_detect", mask[0])
             cv2.waitKey(0)
+            # show fractional filtered image
+            mask = fractional_edge_dft(img, 0.75)
+            cv2.imshow("edge_detect", mask[0])
+            cv2.waitKey(0)
+            showPoints(getCloud(mask[0][crop:len(mask[0][1:])-crop,crop:len(mask[0][:1])-crop]))
 
+            cv2.waitKey(0)
 
 
 
@@ -27,19 +63,16 @@ def showPollen():
 def fractional_edge_dft(gray, alpha=0.7):
 
     f = gray.astype(np.float32)
-
-    #Compute fft
     F = np.fft.fft2(f)
-    #Center data
     Fshift = np.fft.fftshift(F)
 
-    #generate frequency grids
+    #generate frequency grids in freq. dom
     rows, cols = gray.shape
     u = np.linspace(-0.5, 0.5, cols, endpoint=False)
     v = np.linspace(-0.5, 0.5, rows, endpoint=False)
     U, V = np.meshgrid(u, v)
 
-    #Compute Differentiation masks
+    #Compute Differentiation mask (freq interval of 2pi)
     kx = 2 * np.pi * U
     ky = 2 * np.pi * V
     epsilon = 1e-12
@@ -51,18 +84,13 @@ def fractional_edge_dft(gray, alpha=0.7):
     # Apply fractional differentiation masks
     Fx = Fshift * Dx
     Fy = Fshift * Dy
-
-    # 6. Inverse FFT
     fx = np.fft.ifft2(np.fft.ifftshift(Fx))
     fy = np.fft.ifft2(np.fft.ifftshift(Fy))
 
-    #Compute magnitude via sum of squares
-
+    #Classic edge definition based on dist. formula
     mag = np.sqrt(np.abs(fx)**2 + np.abs(fy)**2)
-
     # Normalize to displayable 0–255
     mag = (255 * mag / mag.max()).astype(np.uint8)
-
     return (mag, U,V)
 
 def camStart():
@@ -80,7 +108,7 @@ def camStart():
         frame = cv2.flip(frame, 1)
         hgt, wid, dep = frame.shape
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        edge = fractional_edge_dft(gray, alpha=0.7)
+        edge = fractional_edge_dft(gray, alpha=1)
 
         cv2.imshow('Edge_Detection, 0.5', edge[0])
         v = cv2.waitKey(5)
